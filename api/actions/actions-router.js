a66d7a9cedb08e6bb2ebf81b3d3fed2fd00e8d11
Erick Gonzalez <ericksinbox@gmail.com>
2021-02-12 11:25:33 -0500

    fleshed out routers

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,73 @@
 // Write your "actions" router here!
+
+const router = require("express").Router();
+const Actions = require("./actions-model.js");
+
+
+router.get("/api/actions", (req, res) => {
+    Actions.get()
+    .then(actions => {
+        res.status(200).json(actions)
+    })
+    .catch(err => {
+        res.status(401).json(err.error)
+    })
+});
+
+router.get("/api/actions/:id", (req, res) => {
+    const { id } = req.params;
+
+    Actions.get(id)
+    .then(actions => {
+        res.status(200).json(actions)
+    })
+    .catch(err => {
+        res.status(401).json(err.error)
+    })
+});
+
+router.post("/api/actions", (req, res) => {
+    const { action } = req.body;
+
+    Actions.insert(action)
+    .then(actions => {
+        res.status(200).json(actions)
+    })
+    .catch(err => {
+        res.status(401).json(err.error)
+    })
+});
+
+router.put("/api/actions/:id", (req, res) => {
+    const { id } = req.params;
+    const { changes } = req.body;
+
+    Actions.update(id, changes)
+    .then(actions => {
+        res.status(200).json(actions)
+    })
+    .catch(err => {
+        res.status(401).json(err.error)
+    })
+});
+
+router.delete("/api/actions/:id", (req, res) => {
+    const { id } = req.params;
+
+    Actions.remove(id)
+    .then(actions => {
+        res.status(200).json(actions)
+    })
+    .catch(err => {
+        res.status(401).json(err.error)
+    })
+});
+
+
+
+
+
+
+
+
+module.exports = router;
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,74 @@
 // Write your "projects" router here!
+// Write your "actions" router here!
+
+const router = require("express").Router();
+const Projects = require("./projects-model.js");
+
+
+router.get("/api/projects", (req, res) => {
+    Projects.get()
+    .then(projects => {
+        res.status(200).json(projects)
+    })
+    .catch(err => {
+        res.status(401).json(err.error)
+    })
+});
+
+router.get("/api/projects/:id", (req, res) => {
+    const { id } = req.params;
+
+    Projects.get(id)
+    .then(projects => {
+        res.status(200).json(projects)
+    })
+    .catch(err => {
+        res.status(401).json(err.error)
+    })
+});
+
+router.post("/api/projects", (req, res) => {
+    const { action } = req.body;
+
+    Projects.insert(action)
+    .then(projects => {
+        res.status(200).json(projects)
+    })
+    .catch(err => {
+        res.status(401).json(err.error)
+    })
+});
+
+router.put("/api/projects/:id", (req, res) => {
+    const { id } = req.params;
+    const { changes } = req.body;
+
+    Projects.update(id, changes)
+    .then(projects => {
+        res.status(200).json(projects)
+    })
+    .catch(err => {
+        res.status(401).json(err.error)
+    })
+});
+
+router.delete("/api/projects/:id", (req, res) => {
+    const { id } = req.params;
+
+    Projects.remove(id)
+    .then(projects => {
+        res.status(200).json(projects)
+    })
+    .catch(err => {
+        res.status(401).json(err.error)
+    })
+});
+
+
+
+
+
+
+
+
+module.exports = router;
